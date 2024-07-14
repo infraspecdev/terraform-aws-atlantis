80bd12e7bf664dd9429ede6a58e2534c1a021bd7
nimisha-gj <nimisha@infraspec.dev>
2024-07-14 13:09:38 +0530

    fix: remove provider for network module

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -16,12 +16,6 @@ variable "private_subnet_cidrs" {
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
-variable "region" {
-  description = "Region for creating resources"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "availability_zones" {
   description = "List of Availability zone where the subnet must reside."
   type        = list(string)
